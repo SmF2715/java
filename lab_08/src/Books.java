d1d2e46192a1907097c27fb687a9bb3570791a64
SmF2715 <112180191+SmF2715@users.noreply.github.com>
2023-02-21 17:41:04 +0300

    starting of lab08

---
+++ lab_08/src/Books.java
@@ -0,0 +1,67 @@
+import java.sql.SQLOutput;
+
+public class Books extends PublishingHouse{
+    private String author;
+    private String name;
+    private int pages;
+    int volumes;
+
+    public Books(){
+
+    }
+
+    public Books(String author, String name, int pages, int volumes){
+        this.author = author;
+        this.name = name;
+        this.pages = pages;
+        this.volumes = volumes;
+        super.setPublisher("Неизвестен");
+        super.setYearOfPublishing(0);
+    }
+
+    public Books(String publisher, int yearOfPublishing, String author, String name, int pages, int volumes){
+        super(publisher, yearOfPublishing);
+        this.author = author;
+        this.name = name;
+        this.pages = pages;
+        this.volumes = volumes;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public int getVolumes() {
+        return volumes;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void setVolumes(int volumes) {
+        this.volumes = volumes;
+    }
+
+    @Override
+    public void PrintInfo() {
+        System.out.printf("Автор: %s \nНазвание книги: %s \nСтраниц: %d \nТом: %d",author, name, pages, volumes);
+        super.PrintInfo();
+    }
+}
