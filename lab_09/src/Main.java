ed99f76e8063042b5df11926d86a1ed78e6d54b8
SmF2715 <yarack27@mail.ru>
2023-02-28 18:20:24 +0300

    lab_09 complete

--- lab_09/src/Main.java
+++ lab_09/src/Main.java
@@ -1,6 +1,16 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Лабораторная работа №9 \nВариант №4 \n");
-            
+
+        try(FileReader reader = new FileReader("lab_09.txt")){
+            int c;
+            while ((c= reader.read())!= -1){
+                System.out.print((char)c);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
