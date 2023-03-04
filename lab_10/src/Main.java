de8c6acfedc94058c8ead634eeef4908b738236e
SmF2715 <112180191+SmF2715@users.noreply.github.com>
2023-03-04 12:00:34 +0300

    start lab_10, first, second and fifth task completed

--- lab_10/src/Main.java
+++ lab_10/src/Main.java
@@ -1,5 +1,4 @@
 import java.lang.String;
-import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -10,107 +9,7 @@ public class Main {
         System.out.println("Лабораторная работа №10 \nВариант №4 \n");
         StringUtils test = new StringUtils();
         test.printText();
-        test.setInputString();
-        test.searchInText();
-        System.out.println(test.wordCounter());
-
-
-        /*String text = "";
-        try (FileReader reader = new FileReader("main.txt")){
-            int c;
-
-            while ((c = reader.read())!= -1){
-                text += ((char)c);
-            }
-
-            System.out.println("Текст из файла: \n\n" + text);
-        } catch (FileNotFoundException e) {
-            System.out.println("Ошибка: \n" + e.getMessage());
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-        System.out.print("\nВведите текст, который надо найти в тексте: ");
-        String text_input = scanner.nextLine();
-        text = text.replaceAll("\r\n", " ");
-        searchInText(text, text_input);
-        System.out.println("\nКоличество слов в тексте: " + wordCounter(text));
-        System.out.println("\nКоличество символов в тексте (вместе с пробелами): " + symbolsWithSpacesCounter(text));
-        System.out.println("\nКоличество символов в тексте (без пробелов): " + symbolsWithoutSpacesCounter(text));
-        System.out.println("\nКоличество гласных в тексте: " + vowelsCounter(text));
-        System.out.println("\nКоличество согласных в тексте: " + consonantsCounter(text));
-        System.out.println("\nСамое короткое слово в тексте: " + shortestWord(text));
-        System.out.println("\nСамое длинное слово в тексте: " + longestWord(text));
-   */ }
-
-    public static void searchInText(String text, String text_input){
-        if(text.indexOf(text_input)!=-1){
-            System.out.printf("Подстрока \"%s\" присутствует в данном тексте\n", text_input);
-        }
-        else{
-            System.out.printf("Подстрока \"%s\" не присутствует в данном тексте\n", text_input);
-        }
-    }
-
-    public static int wordCounter(String text){
-        return text.split(" ").length;
-    }
-
-    public static int symbolsWithSpacesCounter(String text){
-        return text.length();
-    }
-
-    public static int symbolsWithoutSpacesCounter(String text){
-        return text.replaceAll(" ", "").length();
-    }
-
-    public static int vowelsCounter(String text){
-        int counter = 0;
-        char[] vowels = "ёуеыаоэяию".toCharArray();
-        char[] array = text.toCharArray();
-        for (int i = 0; i < vowels.length; i++){
-            for (int j = 0; j < array.length; j++){
-                if (vowels[i]==array[j]){
-                    counter++;
-                }
-            }
-        }
-        return counter;
-    }
-
-    public static int consonantsCounter(String text){
-        int counter = 0;
-        char[] consonants = "йцкнгшщзхъфвпрлджчмтьб".toCharArray();
-        char[] array = text.toCharArray();
-        for (int i = 0; i < consonants.length; i++){
-            for (int j = 0; j < array.length; j++){
-                if (consonants[i]==array[j]){
-                    counter++;
-                }
-            }
-        }
-        return counter;
-    }
-
-    public static String shortestWord(String text){
-        String[] array = text.split(" ");
-        String shortestWord = array[0];
-        for (int i = 0; i < array.length; i++){
-            if (shortestWord.length() > array[i].length()){
-                shortestWord = array[i];
-            }
-        }
-        return shortestWord;
-    }
-
-    public static String longestWord(String text){
-        String[] array = text.split(" ");
-        String longestWord = array[0];
-        for (int i = 0; i < array.length; i++){
-            if (longestWord.length() < array[i].length()){
-                longestWord = array[i];
-            }
-        }
-        return longestWord;
+        test.writeInFile();
+        test.printText();
     }
 }
--- lab_10/src/StringUtils.java
+++ lab_10/src/StringUtils.java
@@ -31,17 +31,29 @@ public class StringUtils {
         catch (IOException e){
             System.out.println(e.getMessage());
         }
-        mainString.replaceAll("\r\n", " ");
+        mainString.replaceAll("[\r\n]", " ");
         return  mainString;
     }
 
+    public static void writeInFile(){
+        try(FileWriter fileWriter = new FileWriter("main.txt", false)){
+            System.out.println("Введите текст для записи в файл");
+            String inputInFile = scanner.nextLine();
+            fileWriter.append(" " + inputInFile + " ");
+            fileWriter.flush();
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void printText(){
-        System.out.println("Текст: " + readFromFile());
+        System.out.println("Текст: \n" + readFromFile());
     }
 
     public static void searchInText(){
         if(readFromFile().indexOf(inputString)!=-1){
-            System.out.printf("Подстрока \"%s\" присутствует в данном тексте в \n", inputString);
+            System.out.printf("Подстрока \"%s\" присутствует в данном тексте \n", inputString);
         }
         else{
             System.out.printf("Подстрока \"%s\" не присутствует в данном тексте\n", inputString);
@@ -49,6 +61,81 @@ public class StringUtils {
     }
 
     public static int wordCounter(){
-        return  129408;
+        return readFromFile().split(" ").length;
+    }
+
+    public static int symbolsWithSpacesCounter(){
+        return readFromFile().length();
+    }
+
+    public static int symbolsWithoutSpacesCounter() {
+        return readFromFile().replaceAll(" ", "").length();
+    }
+
+    public static int vowelsCounter(){
+        int counter = 0;
+        char[] vowels = "ёуеыаоэяию".toCharArray();
+        char[] array = readFromFile().toCharArray();
+        for (int i = 0; i < vowels.length; i++){
+            for (int j = 0; j < array.length; j++){
+                if (vowels[i]==array[j]){
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    public static int consonantsCounter(){
+        int counter = 0;
+        char[] consonants = "йцкнгшщзхъфвпрлджчмтьб".toCharArray();
+        char[] array = readFromFile().toCharArray();
+        for (int i = 0; i < consonants.length; i++){
+            for (int j = 0; j < array.length; j++){
+                if (consonants[i]==array[j]){
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    public static String formatedString(){
+        return readFromFile().replaceAll("[\r\n]"," ");
+    }
+
+    public static String shortestWord(){
+        String[] array = readFromFile().split(" ");
+        String shortestWord = array[0];
+        for (int i = 0; i < array.length; i++){
+            if (shortestWord.length() > array[i].length()){
+                shortestWord = array[i];
+            }
+        }
+        return shortestWord;
+    }
+
+    public static String longestWord(){
+        String[] array = readFromFile().split(" ");
+        String longestWord = array[0];
+            for (int i = 0; i < array.length; i++){
+            if (longestWord.length() < array[i].length()){
+                longestWord = array[i];
+            }
+        }
+        return longestWord;
+    }
+
+    public static void printALlMethods(){
+        printText();
+        setInputString();
+        searchInText();
+        System.out.println("Кол-во слов: "+ wordCounter());
+        System.out.println("Кол-во символов (с пробелами): " + symbolsWithSpacesCounter());
+        System.out.println("Кол-во символов (без пробелов): " + symbolsWithoutSpacesCounter());
+        System.out.println("Кол-во гласных в тексте: " + vowelsCounter());
+        System.out.println("Кол-во согласных в тексте: " + consonantsCounter());
+        System.out.println("Самое короткое слово в тексте: "+ shortestWord());
+        System.out.println("Самое длиное слово в тексте: "+ longestWord());
     }
 }
