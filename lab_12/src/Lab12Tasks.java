62e5d42784b0f142877f1e292b55a275a90365c4
sl <aaazzz200407151040@gmail.com>
2023-03-13 20:38:34 +0300

    ui

--- lab_12/src/Lab12Tasks.java
+++ lab_12/src/Lab12Tasks.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Lab12Tasks {
+
+    public static void task1(){
+        System.out.println("Задание №1\n");
+        String inputString = new Scanner(System.in).nextLine();
+
+    }
+
     public static void main(String[] args){
-        System.out.println("Лабораторная работа №12 \nВариант 14 \n");
+        System.out.println("Лабораторная работа №12 \n");
+        task1();
     }
 }
